f391d7de8e8510dfc1d2e68854b1fb4ecbc61061
sxhw2021 <80562670+sxhw2021@users.noreply.github.com>
2026-01-30 16:31:28 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.github.proify.saltadapter"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.proify.saltadapter"
         minSdk = 27
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
