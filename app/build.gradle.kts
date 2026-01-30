551a1291e2391f77b62fff2a0204c62d004de3c6
sxhw2021 <80562670+sxhw2021@users.noreply.github.com>
2026-01-30 15:53:37 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "io.github.proify.saltadapter"
-    compileSdk = libs.versions.compileSdk.get().toInt()
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.proify.saltadapter"
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
+        minSdk = 27
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
@@ -37,8 +37,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.lyricon.provider)
-    implementation(libs.yuki.hook.api)
-    ksp(libs.yuki.hook.ksp)
-    implementation(libs.androidx.core.ktx)
+    implementation("io.github.proify.lyricon:provider:0.1.64")
+    implementation("com.highcapable.yukihookapi:api:1.2.1")
+    ksp("com.highcapable.yukihookapi:ksp-xposed:1.2.1")
+    implementation("androidx.core:core-ktx:1.13.1")
 }
