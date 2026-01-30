f0e892b22ddd4577ddbf30cc9edd884b5afd3671
sxhw2021 <80562670+sxhw2021@users.noreply.github.com>
2026-01-30 16:52:31 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
     
     dependencies {
         classpath("com.android.tools.build:gradle:8.9.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.27")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.1.0-1.0.29")
     }
 }
 
